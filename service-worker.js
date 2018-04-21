23f214d3a30b25a222cf9568e81fa15c7a4ccc41
Paolo Barbolini <paolo@paolo565.org>
2018-04-21 22:09:11 +0200

    Fix the service worker

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,9 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
             return response || fetch(event.request).then(response => {
-                cache.put(event.request, response.clone());
+                caches.open(cacheName).then(cache => {
+                    cache.put(event.request, response.clone());
+                });
 
                 return response;
             });
