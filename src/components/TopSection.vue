49119b63a30bc6dfddbf5f08bb2bd54e99992e0f
Paolo Barbolini <paolo@paolo565.org>
2018-10-26 20:56:06 +0200

    I promise this is the last commit about sharing

--- src/components/TopSection.vue
+++ src/components/TopSection.vue
@@ -60,7 +60,7 @@ export default {
     doShare() {
       navigator.share({
         title: 'Istituto Gobetti App',
-        text: 'Controlla con facilità gli orari e gli avvisi' +
+        text: 'Controlla con facilità gli orari e gli avvisi ' +
               'dell\'Istituto Gobetti',
         url: 'https://school.paolo565.org',
       });
