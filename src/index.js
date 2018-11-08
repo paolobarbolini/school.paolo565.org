49d29b0e0068217525a2cb49767e121d8c80d0ab
Paolo Barbolini <paolo@paolo565.org>
2018-11-08 14:21:03 +0100

    Lighthouse thought that the service worker wasn't getting registered

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import runtime from 'serviceworker-webpack-plugin/lib/runtime';
 import 'request-idle-polyfill'; // Edge Browser is evil
 import './index.scss';
 
+runtime.register();
+
 window.addEventListener('load', () => {
   new Vue({
     router,
     render: (h) => h(App),
   }).$mount('#app');
-
-  runtime.register();
 });
