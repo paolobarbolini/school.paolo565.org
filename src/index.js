05189f160696deec52ba7e1e80e61365bd640c66
Paolo Barbolini <paolo@paolo565.org>
2018-11-29 20:05:14 +0100

    Add sentry for error logging

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import router from './router';
 import App from './App';
 import runtime from 'serviceworker-webpack-plugin/lib/runtime';
 
+import * as Sentry from '@sentry/browser';
+
 import 'request-idle-polyfill'; // Edge Browser is evil
 import './index.scss';
+
+Sentry.init({
+  dsn: 'https://7c001b53eb4b459d8eeb8806eaa7b898@sentry.io/1333778',
+  integrations: [new Sentry.Integrations.Vue({Vue})],
+});
 
 runtime.register();
 
