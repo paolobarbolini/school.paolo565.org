867980510bc4fdeb0cdb6a08829cdd96f7d4478c
Paolo Barbolini <paolo@paolo565.org>
2019-09-12 18:37:00 +0200

    add search functionality

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -13,7 +13,7 @@ if ('serviceWorker' in navigator) {
 /* ============================================================ */
 
 if (navigator.share) {
-    document.addEventListener('load', () => {
+    document.addEventListener('DOMContentLoaded', () => {
         const share = document.querySelector('#web-share');
         share.addEventListener('click', () => {
             navigator.share({
@@ -26,3 +26,45 @@ if (navigator.share) {
         share.classList.remove('hidden');
     });
 }
+
+/* ============================================================ */
+/* Search Bar */
+/* ============================================================ */
+
+document.addEventListener('DOMContentLoaded', () => {
+    const search = document.querySelector('#search');
+    if (!search) return;
+
+    // Display the search input
+    const searchBar = document.querySelector('#search-bar');
+    search.addEventListener('click', () => {
+        searchBar.classList.add('active');
+    });
+
+    // Hide search input
+    const searchInput = document.querySelector('#search-bar input');
+    searchInput.addEventListener('keydown', (event) => {
+        if (event.key !== 'Escape' && event.key !== 'Enter') return;
+
+        searchBar.classList.remove('active');
+    });
+
+    // Search
+    searchInput.addEventListener('input', (event) => {
+        const value = event.target.value;
+        const elements = document.querySelectorAll('.column-item');
+
+        for (const element of elements) {
+            if (!value) {
+                element.classList.remove('hidden');
+                continue;
+            }
+
+            if (element.innerText.includes(value)) {
+                element.classList.remove('hidden');
+            } else {
+                element.classList.add('hidden');
+            }
+        }
+    });
+});
