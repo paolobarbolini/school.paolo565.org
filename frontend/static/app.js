682010a82b45c2ddf3c1abb83e95e974b50df4fd
Paolo Barbolini <paolo@paolo565.org>
2019-09-12 17:48:56 +0200

    implement page sharing via the web share api

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -3,7 +3,26 @@
 /* ============================================================ */
 
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
+    window.addEventListener('load', () => {
         navigator.serviceWorker.register('/service-worker.js');
+    });
+}
+
+/* ============================================================ */
+/* Page Sharing */
+/* ============================================================ */
+
+if (navigator.share) {
+    document.addEventListener('load', () => {
+        const share = document.querySelector('#web-share');
+        share.addEventListener('click', () => {
+            navigator.share({
+                title: document.title,
+                text: 'Controlla con facilità gli orari e gli avvisi dell\'Istituto Gobetti',
+                url: window.location.href ,
+            });          
+        });
+        
+        share.classList.remove('hidden');
     });
 }
