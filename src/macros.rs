3c1e19b35191e648c609cf8664aa658ce9d99229
Paolo Barbolini <paolo@paolo565.org>
2019-09-14 11:16:34 +0200

    fixup render hour

--- src/macros.rs
+++ src/macros.rs
@@ -11,3 +11,16 @@ macro_rules! load_hours {
         };
     };
 }
+
+macro_rules! render_hour {
+    ($base: ident, $kind: tt, $hour: ident) => {
+        let html = $hour.html(&$base).unwrap();
+
+        let mut map = HashMap::new();
+        map.insert("hour", json!($hour));
+        map.insert("hour_html", json!(html));
+        map.insert("path", json!(format!("/{}/{}", $kind, $hour.title)));
+        map.insert("is_index", json!(true));
+        return handlebars_response!(disable_minify "hour", &map);
+    };
+}
