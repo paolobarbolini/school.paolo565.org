184616f6a9b8401d5a4c80bcf227a89fdd134e60
Paolo Barbolini <paolo@paolo565.org>
2019-12-30 19:43:13 +0100

    chore: use a static http client and set the User-Agent header

--- src/cache.rs
+++ src/cache.rs
@@ -1,6 +1,26 @@
 use crate::error::{Error, Result};
 use cached::TimedCache;
-use reqwest::blocking::Response;
+use reqwest::{
+    blocking::{Client, ClientBuilder, Response},
+    header::{HeaderMap, HeaderValue, USER_AGENT},
+};
+
+lazy_static::lazy_static! {
+    static ref HTTP_CLIENT: Client = ClientBuilder::new()
+        .default_headers({
+            let mut headers = HeaderMap::new();
+            headers.insert(
+                USER_AGENT,
+                HeaderValue::from_static(concat!(
+                    "school.paolo565.org/2.0",
+                    " (https://github.com/paolobarbolini/school.paolo565.org)"
+                )),
+            );
+            headers
+        })
+        .build()
+        .unwrap();
+}
 
 cached! {
     STRING_CACHE: TimedCache<String, Result<String>> = TimedCache::with_lifespan_and_capacity(60 * 20, 200);
@@ -23,7 +43,7 @@ cached! {
 }
 
 fn request(url: String) -> Result<Response> {
-    let resp = reqwest::blocking::get(&url)?;
+    let resp = HTTP_CLIENT.get(&url).send()?;
     if !resp.status().is_success() {
         return Err(Error::UnexpectedStatusCode {
             status: resp.status(),
